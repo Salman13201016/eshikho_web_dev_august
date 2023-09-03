a518d47cadc5793a8b935d74ce2e3787f4e073a8
salman <salmanmdsultan@gmail.com>
2023-09-03 16:25:21 +0600

    basic 1

---
+++ js/basic.js
@@ -0,0 +1,24 @@
+var price = 10
+
+// Arithmatic Operator
+
+var prod1 = 100
+var quan_prod1 = 3
+
+var prod2 = 200
+var quan_prod2 = 2
+
+total = (quan_prod1 * prod1) + (quan_prod2 * prod2)
+
+discount_price = total - (total * 0.05)
+// % = modulas -> Remainder 
+remainder = 10 % 9
+
+square = 10 ** 2
+qube = 10 ** 3
+
+// 5 % discount
+
+
+console.log(qube)
+
